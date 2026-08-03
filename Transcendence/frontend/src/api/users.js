fca4e5dd20f3934f7852db1b44778af9f868abca
camansou <camansou@student.42.fr>
2026-08-03 16:02:39 +0200

    Page Profil edition nom statut et langue OK

--- Transcendence/frontend/src/api/users.js
+++ Transcendence/frontend/src/api/users.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { mockUsers } from "../data/mockUsers";
 import { apiRequest } from "./client";
 
@@ -10,9 +11,29 @@ export function getUsers() {
   return apiRequest("/users");
 }
 
+// PLus utilise ?
 export function getUserById(id) {
   if (USE_MOCK) {
     return mockUsers.find((u) => u.id === id) ?? null;
   }
   return apiRequest(`/users/${id}`);
+}
+
+// envoie au back
+export async function updateProfile(data) {
+  if (USE_MOCK)
+    return {...data}
+
+  const response = await apiRequest(`/profile`, {
+    method: 'PATCH',
+    body: JSON.stringify(data)
+  })
+  return response.user
+}
+
+// recup le back
+export function getProfile() {
+  if (USE_MOCK)
+    return { pseudo: 'alice', email: '...', statut: 'Available', langue: 'fr' }
+  return apiRequest(`/profile`)
 }
--- Transcendence/frontend/src/pages/Profil.jsx
+++ Transcendence/frontend/src/pages/Profil.jsx
@@ -1,31 +1,85 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useAuth } from '../context/AuthContext'
 import Avatar from '../components/ui/Avatar'
 import Card from '../components/ui/Card'
 import Button from '../components/ui/Button'
 import Modal from '../components/ui/Modal'
 import Input from '../components/ui/Input'
+import { getProfile, updateProfile } from '../api/users'
 
 function Profil() {
   const { user, login } = useAuth()
   const [isModalOpen, setIsModalOpen] = useState(false)
-  const [newUsername, setNewUsername] = useState(user?.pseudo || '')
+  const [profile, setProfile] = useState(null) // Cree un profile vide. null au cas ou le profil n'a pas fini de charger(voir plus bas)
+  
+  // Les etats des champs du profil
+  const [pseudo, setPseudo] = useState('')
+  const [statut, setStatut] = useState('')
+  const [language, setLanguage] = useState('')
+  //const [avatar, setAvatar] = useState(null)
 
-  const handleSave = () => {
-    login({ ...user, username: newUsername })
+  // Save les erreurs des champsdu profile
+  const [error, setError] = useState('')
+
+  async function handleSave () {
+    try {
+      setError('') // vide le precendent message d'erreur
+      const data = await updateProfile({pseudo, statut, langue: language})
+      login(data)
+      setProfile(data) // met a jour instantannement apres les chnagement la carte profile
+      setIsModalOpen(false)
+    } catch (err) {
+      setError(err.message) // le back a ecrit l'erreur. On l'affiche
+    }
+  }
+
+  
+
+  async function loadProfile() {
+    try {
+      const data = await getProfile()
+      setProfile(data) // On remplit le tableau 
+    } catch (error) {
+      console.error('Impossible de charger le profile', error)
+    }
+  }
+
+  // charge mon profile au montage
+  useEffect(() => {
+    loadProfile()
+  }, [])
+
+
+  // ce bloc s'exécute à chaque fois que profile change
+  useEffect(() => {
+   if (profile) {
+    // On recup donnes du back. On save les nouvelles valeurs mais on ne les echange pas direct. 
+    // Au cas ou on annule il faut se rapler des anciennes valeurs
+    setPseudo(profile.pseudo)
+    setStatut(profile.statut)
+    setLanguage(profile.langue)
+   }
+  }, [profile])   // le tableau de dépendances : "surveille profile"
+
+  function handleCancel() {
+    setPseudo(profile.pseudo)
+    setStatut(profile.statut)
+    setLanguage(profile.langue)
+    setError('')
     setIsModalOpen(false)
   }
 
-  console.log(user)   // dans MainLayout ou Profil
+  // Tant que le GET n'a pas repondu on attend. Ensuite profile devient l'objet et le vrai ccontenu s'affcihe
+  if (!profile) return <p>Chargmenet...</p>
 
   return (
     <div className="max-w-md mx-auto py-10 px-4">
 
       <Card className="flex items-center gap-4">
-        <Avatar username={user?.pseudo} size="lg" />
+        <Avatar username={profile.pseudo} size="lg" />
         <div>
-          <h1 className="text-xl font-bold text-gray-900">{user?.pseudo}</h1>
-          <p className="text-gray-500 text-sm">{user?.email}</p>
+          <h1 className="text-xl font-bold text-gray-900">{profile.pseudo}</h1>
+          <p className="text-gray-500 text-sm">{profile.email}</p>
         </div>
         <div className="ml-auto">
           <Button onClick={() => setIsModalOpen(true)}>Modifier</Button>
@@ -34,17 +88,40 @@ function Profil() {
 
       <Modal
         isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
+        onClose={handleCancel}
         title="Modifier le profil"
       >
         <Input
           type="text"
-          value={newUsername}
-          onChange={(e) => setNewUsername(e.target.value)}
+          value={pseudo}
+          onChange={(e) => setPseudo(e.target.value)}
           placeholder="Nouveau pseudo"
         />
+        {error && <p className='text-sm text-red-400'>{error}</p>}
+        
+        <select
+          value={statut}
+          onChange={(e) => setStatut(e.target.value)} // e.target.value contient la value de l'option selectionne
+          className='text-sm text-gray-700 border border-gray-200 rounded-lg px-2 py-1'
+        >
+          <option value="Available">Disponible</option>
+          <option value="Busy">Occupe</option>
+          <option value="Away">Absent</option>
+        </select>
+
+        <select
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)} // e.target.value contient la value de l'option selectionne
+          className='text-sm text-gray-700 border border-gray-200 rounded-lg px-2 py-1'
+        >
+          <option value="fr">Francais</option>
+          <option value="en">Anglais</option>
+          <option value="cn">Chinois</option>
+          
+        </select>
+        
         <div className="flex justify-end gap-2 mt-4">
-          <Button onClick={() => setIsModalOpen(false)}>Annuler</Button>
+          <Button onClick={handleCancel}>Annuler</Button>
           <Button onClick={handleSave}>Sauvegarder</Button>
         </div>
       </Modal>
