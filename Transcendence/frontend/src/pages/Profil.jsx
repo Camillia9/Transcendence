4ebbc45d942a99e71737f1b04b14b63f16964983
camansou <camansou@student.42.fr>
2026-08-04 17:42:22 +0200

    Page Profil Fini Ok

--- Transcendence/frontend/src/pages/Profil.jsx
+++ Transcendence/frontend/src/pages/Profil.jsx
@@ -3,13 +3,11 @@ import { useAuth } from '../context/AuthContext'
 import Avatar from '../components/ui/Avatar'
 import Card from '../components/ui/Card'
 import Button from '../components/ui/Button'
-import Modal from '../components/ui/Modal'
 import Input from '../components/ui/Input'
 import { changePassword, getProfile, updateProfile } from '../api/users'
 
 function Profil() {
   const { user, login } = useAuth()
-  const [isModalOpen, setIsModalOpen] = useState(false)
   const [profile, setProfile] = useState(null) // Cree un profile vide. null au cas ou le profil n'a pas fini de charger(voir plus bas)
   
   // Les etats des champs du profil
@@ -32,7 +30,6 @@ function Profil() {
       const data = await updateProfile({pseudo, statut, langue: language})
       login(data)
       setProfile(data) // met a jour instantannement apres les chnagement la carte profile
-      setIsModalOpen(false)
     } catch (err) {
       setError(err.message) // le back a ecrit l'erreur. On l'affiche
     }
@@ -69,7 +66,6 @@ function Profil() {
     setStatut(profile.statut)
     setLanguage(profile.langue)
     setError('')
-    setIsModalOpen(false)
   }
 
   // Gestion du MDP 
@@ -80,14 +76,13 @@ function Profil() {
       setOldPassword('') // vide l'ancien state
       setNewPassword('') // vide l'ancien satate
       setPasswordSuccess('Mot de passe change')
-      setIsModalOpen(false)
     } catch (err) {
       setPasswordError(err.message) // le back a ecrit l'erreur. On l'affiche
     }
   }
 
   // Tant que le GET n'a pas repondu on attend. Ensuite profile devient l'objet et le vrai ccontenu s'affcihe
-  if (!profile) return <p>Chargmenet...</p>
+  if (!profile) return <p>Chargement...</p>
 
   return (
     <div className="max-w-lg mx-auto flex flex-col gap-6 py-10">
@@ -102,45 +97,89 @@ function Profil() {
         </div>
       </Card>
 
-      <Modal
-        isOpen={isModalOpen}
-        onClose={handleCancel}
-        title="Modifier le profil"
-      >
-        <Input
-          type="text"
-          value={pseudo}
-          onChange={(e) => setPseudo(e.target.value)}
-          placeholder="Nouveau pseudo"
-        />
+      <Card className='flex flex-col gap-4'>
+        {/*PSeudo */}
+        <div className='flex items-center justify-between'>
+          <label className='text-sm text-gray-500'>Pseudo</label>
+            <Input
+              type="text"
+              value={pseudo}
+              onChange={(e) => setPseudo(e.target.value)}
+              placeholder="Nouveau pseudo"
+              className="w-48"
+          />
+        </div>
+
+        {/*Statut */}
+        <div className='flex items-center justify-between'>
+          <label className='text-sm text-gray-500'>Statut</label>
+            <select
+              value={statut}
+              onChange={(e) => setStatut(e.target.value)} // e.target.value contient la value de l'option selectionne
+              className='text-sm text-gray-700 border border-gray-200 rounded-lg px-2 py-1'
+            >
+              <option value="Available">Disponible</option>
+              <option value="Busy">Occupe</option>
+              <option value="Away">Absent</option>
+            </select>
+        </div>
+
+         {/*Langue */}
+        <div className='flex items-center justify-between'>
+          <label className='text-sm text-gray-500'>Langue</label>
+            <select
+              value={language}
+              onChange={(e) => setLanguage(e.target.value)} // e.target.value contient la value de l'option selectionne
+              className='text-sm text-gray-700 border border-gray-200 rounded-lg px-2 py-1'
+            >
+              <option value="fr">Francais</option>
+              <option value="en">Anglais</option>
+              <option value="cn">Chinois</option>
+
+            </select>
+        </div>
+
         {error && <p className='text-sm text-red-400'>{error}</p>}
         
-        <select
-          value={statut}
-          onChange={(e) => setStatut(e.target.value)} // e.target.value contient la value de l'option selectionne
-          className='text-sm text-gray-700 border border-gray-200 rounded-lg px-2 py-1'
-        >
-          <option value="Available">Disponible</option>
-          <option value="Busy">Occupe</option>
-          <option value="Away">Absent</option>
-        </select>
-
-        <select
-          value={language}
-          onChange={(e) => setLanguage(e.target.value)} // e.target.value contient la value de l'option selectionne
-          className='text-sm text-gray-700 border border-gray-200 rounded-lg px-2 py-1'
-        >
-          <option value="fr">Francais</option>
-          <option value="en">Anglais</option>
-          <option value="cn">Chinois</option>
-          
-        </select>
-        
+        {/*Les boutons */}
         <div className="flex justify-end gap-2 mt-4">
-          <Button onClick={handleCancel}>Annuler</Button>
-          <Button onClick={handleSave}>Sauvegarder</Button>
+          <Button onClick={handleCancel}>Reinitialiser</Button>
+          <Button onClick={handleSave}>Enregistrer</Button>
         </div>
-      </Modal>
+      </Card>
+
+      <Card className='flex flex-col gap-4'>
+        <h2 className='text-sm font-medium text-gray-700'>Modifier le mot de passe</h2>
+        {/*Ancien MDP */}
+        <div className='flex items-center justify-between'>
+          <label className='text-sm text-gray-500'>Mot de passe actuel</label>
+            <Input
+              type="password"
+              value={oldPassword}
+              onChange={(e) => setOldPassword(e.target.value)}
+              placeholder="Mot de passe actuel"
+              className="w-48"
+          />
+        </div>
+
+        {/*Nouveau mot de passe */}
+        <div className='flex items-center justify-between'>
+          <label className='text-sm text-gray-500'>Nouveau mot de passe</label>
+          <Input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              placeholder="Nouveau mot de passe"
+              className="w-48"
+          />
+        </div>
+          {passwordError && <p className="text-sm text-red-400">{passwordError}</p>}
+          <div className='flex justify-end mt-2'>
+            <Button onClick={handleChangePassword}>Changer</Button>
+          </div>
+
+          {passwordSuccess && <p className="text-sm text-green-500">{passwordSuccess}</p>}
+      </Card>
 
     </div>
   )
