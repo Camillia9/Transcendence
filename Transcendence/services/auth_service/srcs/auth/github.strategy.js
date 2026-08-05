abdd0597249823c7c96429c7c17309363569a992
Emmanuel IEONG <eieong@student.42.fr>
2026-08-05 14:53:41 +0200

    changed URL from localhost:3000 to https://localhost

--- Transcendence/services/auth_service/srcs/auth/github.strategy.js
+++ Transcendence/services/auth_service/srcs/auth/github.strategy.js
@@ -6,7 +6,7 @@ const gitHubStrategy = new GitHubStrategy (
     {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: '/auth/github/callback',
+        callbackURL: 'https://localhost/api/auth/github/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
         try{
