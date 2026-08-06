83a654b62a5bd42ea00744b6872f3ad53cbaf757
makoon <makoon@f4r1s6.paris.42.school>
2026-08-06 16:39:06 +0200

    ajout limit taille

--- Transcendence/services/server.js
+++ Transcendence/services/server.js
@@ -39,7 +39,7 @@ app.use(cors({
 }))
 
 // Permet de lire le corps JSON des requetes entrantes (req.body)
-app.use(express.json())
+app.use(express.json({limit: '5mb'}))
 
 // // Passport OAuth
 passport.use(googleStrategy);
@@ -48,6 +48,7 @@ passport.use(gitHubStrategy);
 // // Routes REST authentification / organisation
 app.use('/api', authRouter);
 app.use('/api', orgaRouter);
+app.use('/api', invitationRouter);
 app.use('/api', projectRouter);
 app.use('/api', taskRouter);
 app.use('/api', userRouter);
